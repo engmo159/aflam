c87466dc4abb3a789a9399c32d715edfcdf8f7fd
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-01 23:05:30 +0300

    Mohamed (#16)
    * aaa
    
    * aaa
    
    * ccc
    
    * ss
    
    * ss
    
    * theme reducer make
    
    * navbar
    
    * a
    
    * a
    
    ---------
    
    Co-authored-by: Mohammed <engmo159@git.com>

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Movies = () => {
   return <div className='pt-16'>Ahmed</div>
 }
--- src/pages/Tv.jsx
+++ src/pages/Tv.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Tv = () => {
   return <div>Tv</div>
 }
