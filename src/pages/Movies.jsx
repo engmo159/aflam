e1e191d80bd5cf9fce13bbf3b610b432ac793ee3
Ahmed Khaled Al-Askary <ahm.alaskary@gmail.com>
2024-10-04 22:38:26 +0300

    fixing hero and genre

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,7 @@
-
 import Hero from "../components/Hero";
 import MoviesSection from "../components/Movies/MoviesSection";
 
-
 const Movies = () => {
-  const { popularMoviesLoading, popularMoviesPage } = useSelector(
-    (state) => state.moviesReducer
-  );
-
   return (
     <div>
       <Hero />
