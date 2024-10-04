b02d8e08402ef794d4a218e92cd5d661be825efb
Ahmed Khaled Al-Askary <ahm.alaskary@gmail.com>
2024-10-04 22:17:56 +0300

    popular movies and top rated movies list in movies page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,24 @@
-
-import Hero from '../components/Hero'
-
+import { useEffect } from "react";
+import Hero from "../components/Hero";
+import MoviesSection from "../components/Movies/MoviesSection";
+import {
+  getGenreMoviesList,
+  getPopularMovies,
+} from "../redux/slices/moviesSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "@material-tailwind/react";
 
 const Movies = () => {
+  const { popularMoviesLoading, popularMoviesPage } = useSelector(
+    (state) => state.moviesReducer
+  );
+
   return (
     <div>
       <Hero />
+      <MoviesSection />
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
