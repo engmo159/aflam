dfbfac322029d06377c21091637dd5e78a74eed5
Ahmed Khaled Al-Askary <114955989+askaryxiii@users.noreply.github.com>
2024-10-04 12:39:40 -0700

    Ahmed (#21)
    * checking github
    
    * popular movies and top rated movies list in movies page
    
    * fixing hero and genre
    
    * fixing hero and genre

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,10 +4,6 @@ import MoviesSection from "../components/Movies/MoviesSection";
 
 
 const Movies = () => {
-  const { popularMoviesLoading, popularMoviesPage } = useSelector(
-    (state) => state.moviesReducer
-  );
-
   return (
     <div>
       <Hero />
