5639542b9bdf724bfdb22c5c18281477608c92c8
Ahmed Khaled Al-Askary <114955989+askaryxiii@users.noreply.github.com>
2024-10-05 12:05:47 -0700

    Ahmed (#28)
    * checking github
    
    * popular movies and top rated movies list in movies page
    
    * fixing hero and genre
    
    * fixing hero and genre
    
    * load more loading animation fixed
    
    * laoding component and fixing some things already done
    
    * laoding component and fixing some things already done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,11 @@
 
 import Hero from "../components/Hero";
-import {  useSelector } from "react-redux";
-import SwiperLayout from '../components/SwiperLayout'
+import { useSelector } from "react-redux";
+import SwiperLayout from "../components/SwiperLayout";
 import Loading from "../components/Loading";
 
 const Home = () => {
   const { pageLoading } = useSelector((state) => state.moviesReducer);
-
-
-const Home = () => {
 
   return (
     <div>
@@ -19,7 +16,9 @@ const Home = () => {
           <Hero />
           <SwiperLayout />
         </div>
-      )}{" "}
+
+      )}
+
     </div>
   );
 };
