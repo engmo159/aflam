fb5a6aa7acb6238e9665c07a6b5578985e39e3fc
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-05 14:56:56 +0300

    Mohamed (#22)
    * aaa
    
    * aaa
    
    * ccc
    
    * ss
    
    * ss
    
    * theme reducer make
    
    * navbar
    
    * a
    
    * a
    
    * finish hero section
    
    * console fix
    
    * f
    
    * f
    
    * make sign in and sign up form
    
    * make sign in and sign up form
    
    ---------
    
    Co-authored-by: Mohammed <engmo159@git.com>

---
+++ src/components/navbar/SignLayout.jsx
@@ -0,0 +1,47 @@
+import { Card, Typography } from '@material-tailwind/react'
+import PropTypes from 'prop-types'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+import { useState } from 'react'
+const SignLayout = ({ showSignInModal, setShowSignInModal }) => {
+  // sign in overlay function
+  const handleOverlayClick = e => {
+    if (e.target.id === 'overlay') {
+      setShowSignInModal(false)
+    }
+  }
+  // signIn State
+  const [showSignIn, setShowSignIn] = useState(true)
+  return (
+    <div>
+      {showSignInModal && (
+        <div
+          id='overlay'
+          onClick={handleOverlayClick}
+          className='fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center '
+        >
+          <Card className='w-1/3 p-8 bg-gray-900 flex gap-4'>
+            <Typography
+              variant='h4'
+              className='py-1.5 font-bold text-2xl
+             text-white text-center'
+            >
+              Redux
+              <span className='text-red-500'> Movies</span>
+            </Typography>
+            {showSignIn ? (
+              <SignIn setShowSignIn={setShowSignIn} />
+            ) : (
+              <SignUp setShowSignIn={setShowSignIn} />
+            )}
+          </Card>
+        </div>
+      )}
+    </div>
+  )
+}
+SignLayout.propTypes = {
+  showSignInModal: PropTypes.bool.isRequired,
+  setShowSignInModal: PropTypes.func.isRequired,
+}
+export default SignLayout
