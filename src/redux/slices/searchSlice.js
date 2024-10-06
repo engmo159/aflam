34eca23e4b50ae8b4d2557ce508735b51a355624
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-07 01:13:01 +0300

    search page

--- src/redux/slices/searchSlice.js
+++ src/redux/slices/searchSlice.js
@@ -14,10 +14,10 @@ const api = (mediaCategory, query, page = 1) => {
 }
 export const getSearchedMedia = createAsyncThunk(
   '/getSearchedMedia',
-  async ({ mediaType, query }, thunkAPI) => {
+  async ({ mediaType, query, page }, thunkAPI) => {
     const { rejectWithValue } = thunkAPI
     try {
-      const { data } = await axios.request(api(mediaType, query))
+      const { data } = await axios.request(api(mediaType, query, page))
       return data
     } catch (e) {
       return rejectWithValue(e)
