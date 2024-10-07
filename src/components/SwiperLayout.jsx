9cd5aba0c871c16ac14d3e20b8dbc720db2ce70a
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-07 03:19:16 +0300

    search page

--- src/components/SwiperLayout.jsx
+++ src/components/SwiperLayout.jsx
@@ -6,14 +6,28 @@ import MovieCard from './Movies/MovieCard'
 
 const SwiperLayout = ({ media, header }) => {
   return (
-    <div className='flex flex-col gap-8 px-20 py-10 h-full overflow-hidden'>
+    <div className='flex flex-col gap-8 px-4 lg:px-20 py-10 h-full overflow-hidden'>
       <div className='flex flex-col gap-1 '>
         <h1 className='font-bold text-3xl'>{header}</h1>
         <hr className='w-28 border-[3px] border-red-600 ' />
       </div>
       <div className='w-full h-full'>
         <Swiper
-          slidesPerView={4}
+          lazy={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+            1280: {
+              slidesPerView: 4,
+            },
+          }}
           centeredSlides={false}
           spaceBetween={0}
           grabCursor={true}
