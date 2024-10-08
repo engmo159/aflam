a504a739d61fb20b7ec933049be2d7c5c5938860
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-08 17:29:36 +0300

    Mohamed (#57)
    * aaa
    
    * aaa
    
    * ccc
    
    * ss
    
    * ss
    
    * theme reducer make
    
    * navbar
    
    * a
    
    * a
    
    * finish hero section
    
    * console fix
    
    * f
    
    * f
    
    * make sign in and sign up form
    
    * make sign in and sign up form
    
    * fix hero width
    
    * add heght to movie card
    
    * add heght to movie card
    
    * add heght to movie card
    
    * fddfd
    
    * add heght to movie card
    
    * add heght to movie card
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    ---------
    
    Co-authored-by: Mohammed <engmo159@git.com>

--- src/pages/MediaDetails.jsx
+++ src/pages/MediaDetails.jsx
@@ -7,15 +7,23 @@ import {
   getCreditDetails,
   getMediaDetails,
 } from '../redux/slices/mediaDetailsSlice'
+import { changePageLoading } from '../redux/slices/moviesSlice'
+import Loading from '../components/Loading'
 const MediaDetails = () => {
   const dispatch = useDispatch()
-  const { mediaDetail } = useSelector(state => state.mediaDetailReducer)
+  const { mediaDetail, mediaDetailLoading } = useSelector(
+    state => state.mediaDetailReducer
+  )
   const { mediaType, mediaId } = useParams()
-
+  const { pageLoading } = useSelector(state => state.moviesReducer)
   useEffect(() => {
+    dispatch(changePageLoading(true))
     dispatch(getMediaDetails({ mediaCategory: mediaType, mediaId }))
     dispatch(getCreditDetails({ mediaCategory: mediaType, mediaId }))
   }, [mediaId, mediaType, dispatch])
+  if (pageLoading) {
+    return <Loading load={mediaDetailLoading} />
+  }
   return (
     <div>
       <MediaHero />
