5c16714b2aadcc6f767bfaaff287f5f8b6dac373
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-10 04:26:56 +0300

    search page

--- src/components/navbar/SignLayout.jsx
+++ src/components/navbar/SignLayout.jsx
@@ -1,33 +1,37 @@
-import { Card, Typography } from "@material-tailwind/react";
-import PropTypes from "prop-types";
-import SignUp from "./SignUp";
-import SignIn from "./SignIn";
-import { useState } from "react";
-const SignLayout = ({ showSignInModal, setShowSignInModal }) => {
+/* eslint-disable react/prop-types */
+import { Card, Typography } from '@material-tailwind/react'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { switchShowSignInModal } from '../../redux/slices/userAuthSlice'
+const SignLayout = () => {
+  const dispatch = useDispatch()
+  const { showSignInModal } = useSelector(state => state.userAuthReducer)
   // sign in overlay function
-  const handleOverlayClick = (e) => {
-    if (e.target.id === "overlay") {
-      setShowSignInModal(false);
+  const handleOverlayClick = e => {
+    if (e.target.id === 'overlay') {
+      dispatch(switchShowSignInModal())
     }
-  };
+  }
   // signIn State
-  const [showSignIn, setShowSignIn] = useState(true);
+  const [showSignIn, setShowSignIn] = useState(true)
   return (
     <div>
       {showSignInModal && (
         <div
-          id="overlay"
+          id='overlay'
           onClick={handleOverlayClick}
-          className="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center "
+          className='fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center '
         >
-          <Card className="lg:w-1/3 p-8 bg-gray-900 flex gap-4">
+          <Card className='lg:w-1/3 p-8 bg-gray-900 flex gap-4'>
             <Typography
-              variant="h4"
-              className="py-1.5 font-bold text-2xl
-             text-white text-center"
+              variant='h4'
+              className='py-1.5 font-bold text-2xl
+             text-white text-center'
             >
               Redux
-              <span className="text-ourRed"> Movies</span>
+              <span className='text-ourRed'> Movies</span>
             </Typography>
             {showSignIn ? (
               <SignIn setShowSignIn={setShowSignIn} />
@@ -38,10 +42,7 @@ const SignLayout = ({ showSignInModal, setShowSignInModal }) => {
         </div>
       )}
     </div>
-  );
-};
-SignLayout.propTypes = {
-  showSignInModal: PropTypes.bool.isRequired,
-  setShowSignInModal: PropTypes.func.isRequired,
-};
-export default SignLayout;
+  )
+}
+
+export default SignLayout
