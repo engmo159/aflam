191892f1fdb6a709695416fa7542cfb7aae27804
Eng. Mohamed Saeed <engmo159159@gmail.com>
2024-10-12 05:10:50 +0300

    Mohamed (#76)
    * aaa
    
    * aaa
    
    * ccc
    
    * ss
    
    * ss
    
    * theme reducer make
    
    * navbar
    
    * a
    
    * a
    
    * finish hero section
    
    * console fix
    
    * f
    
    * f
    
    * make sign in and sign up form
    
    * make sign in and sign up form
    
    * fix hero width
    
    * add heght to movie card
    
    * add heght to movie card
    
    * add heght to movie card
    
    * fddfd
    
    * add heght to movie card
    
    * add heght to movie card
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    * search page
    
    ---------
    
    Co-authored-by: Mohammed <engmo159@git.com>

--- src/components/mediaDetails/ReviewCard.jsx
+++ src/components/mediaDetails/ReviewCard.jsx
@@ -1,36 +1,97 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 
-const ReviewCard = ({ review }) => {
-  var newDate = new Date(review.created_at);
+import { Button } from '@material-tailwind/react'
+import { MdDelete } from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  deleteUserReview,
+  getUserReview,
+  setDeletingReviewsLoading,
+} from '../../redux/slices/reviewSlice'
+import Loading from '../Loading'
+import { changePageLoading } from '../../redux/slices/moviesSlice'
+
+const ReviewCard = ({ review, userProps }) => {
+  var newDate = new Date(review.created_at)
+  const { deleteReviewErr, isDeletingReview } = useSelector(
+    state => state.reviewReducer
+  )
+  const { token } = useSelector(state => state.tokenReducer)
+  const { pageLoading } = useSelector(state => state.moviesReducer)
+
+  const dispatch = useDispatch()
+  if (pageLoading) {
+    return <Loading load={''} />
+  }
+
   return (
-    <div className="flex gap-3">
-      <div className="bg-[#9d0a00] text-white min-w-10 h-10 flex justify-center items-center rounded-full">
-        {review.author_details.name
-          .match(/(\b\S)?/g)
-          .join("")
-          .match(/(^\S|\S$)?/g)
-          .join("")
-          .toUpperCase() ||
-          review.author
+    <div className='flex justify-between items-center hover:bg-gray-900 p-4 rounded'>
+      <div className='flex gap-3 '>
+        <div className='bg-[#9d0a00] text-white min-w-10 h-10 flex justify-center items-center rounded-full'>
+          {review.author_details.name
             .match(/(\b\S)?/g)
-            .join("")
+            .join('')
             .match(/(^\S|\S$)?/g)
-            .join("")
-            .toUpperCase()}
+            .join('')
+            .toUpperCase() ||
+            review.author
+              .match(/(\b\S)?/g)
+              .join('')
+              .match(/(^\S|\S$)?/g)
+              .join('')
+              .toUpperCase()}
+        </div>
+        <div className='flex flex-col gap-2'>
+          <span className='text-black dark:text-white text-lg font-semibold'>
+            {review.author}
+          </span>
+          <span className='text-black dark:text-white text-sm'>
+            {newDate.toLocaleString('en-US')}
+          </span>
+          <span className='text-black  dark:text-white text-sm'>
+            {review.content}
+          </span>
+        </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <span className="text-black dark:text-white text-lg font-semibold">
-          {review.author}
-        </span>
-        <span className="text-black dark:text-white text-sm">
-          {newDate.toLocaleString("en-US")}
-        </span>
-        <span className="text-black  dark:text-white text-sm">
-          {review.content}
-        </span>
-      </div>
-    </div>
-  );
-};
+      {userProps && (
+        <>
+          {deleteReviewErr && (
+            <h1 className='text-red-500'>{deleteReviewErr}</h1>
+          )}
+          <Button
+            color='red'
+            onClick={() => {
+              dispatch(
+                setDeletingReviewsLoading({ index: review?.id, loading: true })
+              )
+              dispatch(
+                deleteUserReview({ token, reviewId: String(review?.id) })
+              ).then(() => {
+                dispatch(changePageLoading(true))
 
-export default ReviewCard;
+                dispatch(
+                  setDeletingReviewsLoading({
+                    index: review?.id,
+                    loading: false,
+                  })
+                )
+                dispatch(getUserReview(token))
+              })
+            }}
+          >
+            {isDeletingReview[review?.id] ? (
+              <span className='btnLoader'></span>
+            ) : (
+              <div className='flex items-center text-md gap-2'>
+                <MdDelete className='text-xl' />
+                remove
+              </div>
+            )}
+          </Button>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ReviewCard
